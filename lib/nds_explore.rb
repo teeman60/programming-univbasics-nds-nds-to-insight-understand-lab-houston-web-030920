e0b84834f9ddb7aff469a219e2c2089500f2712d
Taiye Salami <taiye41@gmail.com>
2020-02-23 13:56:51 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -16,6 +16,12 @@ end
 def print_first_directors_movie_titles
   dr = {:name=>"Stephen Spielberg", :movies=>[{:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975}, {:title=>"Close Encounters of the Third Kind", :studio=>"Columbia", :worldwide_gross=>135189114, :release_year=>1977}, {:title=>"Raiders of the Lost Ark", :studio=>"Paramount", :worldwide_gross=>248159971, :release_year=>1981}, {:title=>"E.T. the Extra-terrestrial", :studio=>"Universal", :worldwide_gross=>435110554, :release_year=>1982}, {:title=>"Schindler's List", :studio=>"Universal", :worldwide_gross=>96898818, :release_year=>1993}, {:title=>"Lincoln", :studio=>"Buena Vista", :worldwide_gross=>182207973, :release_year=>2012}]}
   
+  
+  row = 0
+  while row < dr[:movies].length do
+    column = 0
+    while column < dr[:movies][column].length do
+      
   dr[:movies]
   
   dr[:movies][:title]
