fd8971ef60ac3f2bea03fc5413edfebdd58ee428
Taiye Salami <taiye41@gmail.com>
2020-02-23 11:49:55 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -22,4 +22,4 @@ dr = {:name=>"Stephen Spielberg", :movies=>[{:title=>"Jaws", :studio=>"Universal
 {:name=>"Francis Ford Coppola", :movies=>[{:title=>"The Godfather", :studio=>"Paramount", :worldwide_gross=>134966411, :release_year=>1972}, {:title=>"Apocalypse Now", :studio=>"MGM", :worldwide_gross=>83471511, :release_year=>1979}, {:title=>"Apocalypse Now Redux", :studio=>"Miramax", :worldwide_gross=>83471511, :release_year=>2001}, {:title=>"Bram Stoker's Dracula", :studio=>"Columbia", :worldwide_gross=>82522790, :release_year=>1992}, {:title=>"The Godfather Part III", :studio=>"Paramount", :worldwide_gross=>66666062, :release_year=>1990}, {:title=>"Jack", :studio=>"Buena Vista", :worldwide_gross=>58620973, :release_year=>1996}]}
 
 
-pp 'dr'
+pp (dr)
